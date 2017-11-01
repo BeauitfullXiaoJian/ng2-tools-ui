419da122c5179b41c589ef76fe9f23ec7d29808e
shaobo <929029960@qq.com>
2017-11-01 20:14:53 +0800

    add confirm test

--- example/src/app/tools-ui/components/confirm/confirm.component.ts
+++ example/src/app/tools-ui/components/confirm/confirm.component.ts
@@ -4,20 +4,44 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'ts-confirm',
   template: `
-  <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
-    <div class="card-header">Header
-    <button type="button" class="close pull-right text-light" aria-label="Close">
-      <span aria-hidden="true">&times;</span>
-    </button>
-    </div>
-    <div class="card-body">
-      <h4 class="card-title">Primary card title</h4>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <div class="d-flex fixed-top h-100 w-100" [ngStyle]="{'background-color':bgColor}">
+    <div class="align-self-center w-100">
+      <div class="card {{cardClass}}" style="max-width: 20rem;margin:auto;">
+        <div class="card-header">{{title}}
+          <button type="button" class="close pull-right {{closeClass}}" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="card-body">
+          <p class="card-text">{{message}}</p>
+        </div>
+      </div>
     </div>
   </div>
+      
   `,
+  styles: [
+    '.close {cursor:pointer;}'
+  ]
 })
 export class ConfirmComponent {
-  @Input() name: string
-  @Input() size: string
+
+  @Input() title: string
+
+  @Input() message: string
+
+  @Input() bgColor: string
+
+  @Input() cardClass: string
+
+  @Input() closeClass: string
+
+
+  constructor() {
+    this.title = "Title"
+    this.message = "Some quick example text to build on the card title and make up the bulk of the card's content."
+    this.bgColor = "rgba(0,0,0,0.2)"
+    this.cardClass = "bg-primary text-white"
+    this.closeClass = ""
+  }
 }
